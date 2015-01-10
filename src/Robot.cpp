3995a40616ef20558791dbe5819a29e9dc83367b
Alexander Meyer <alexander.dude.meyer@gmail.com>
2015-01-10 11:25:19 -0600

    Initial commit, hopefully with the right Eclipse files.

---
+++ src/Robot.cpp
@@ -0,0 +1,31 @@
+#include "Robot.h"
+
+Robot::Robot()
+{
+}
+
+void Robot::RobotInit()
+{
+}
+
+void Robot::AutonomousInit()
+{
+}
+
+void Robot::AutonomousPeriodic()
+{
+}
+
+void Robot::TeleopInit()
+{
+}
+
+void Robot::TeleopPeriodic()
+{
+}
+
+void Robot::TestPeriodic()
+{
+}
+
+START_ROBOT_CLASS(Robot);
